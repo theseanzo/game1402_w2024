178b806b7624244761a2bea9652cb811022a3e22
theseanzo <sean.zo.lynch@gmail.com>
2024-04-12 07:54:20 -0600

    Changes from lecture 11

--- Lectures/L11/Assets/UI/Scripts/TitleScreen.cs
+++ Lectures/L11/Assets/UI/Scripts/TitleScreen.cs
@@ -6,5 +6,31 @@ using UnityEngine.UIElements;
 
 public class TitleScreen : MonoBehaviour
 {
-  
+    private VisualElement titleText, parentElement, root;
+    private Button startButton;
+    [SerializeField]
+    AudioSource backgroundMusic;
+    private void Start()
+    {
+        root = GetComponent<UIDocument>().rootVisualElement;
+        titleText = root.Q<VisualElement>("TitleBox");
+        startButton = root.Q<Button>("StartButton");
+        Invoke("TextFlyIn", .2f);
+        startButton.RegisterCallback<TransitionEndEvent>(evt =>
+        {
+            startButton.ToggleInClassList("fadeBackground");
+            startButton.ToggleInClassList("brightenBackground");
+        });
+        root.schedule.Execute(() =>
+        {
+            backgroundMusic.Play();
+            startButton.ToggleInClassList("fadeBackground");
+            startButton.ToggleInClassList("brightenBackground");
+        }).StartingIn(5000);
+    }
+    private void TextFlyIn()
+    {
+        titleText.RemoveFromClassList("flyIn");
+        startButton.RemoveFromClassList("flyIn");
+    }
 }
